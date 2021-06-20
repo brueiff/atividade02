6d95913beee76e25bcc13567c8efde281b304f06
Bruno alves <brunooalvves123@gmail.com>
2021-06-20 14:47:42 -0300

    ola

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>ola</title>
 </head>
 <body>
- <h3>ola</h3>  
+ 
  <?php
  <a href="crud/create.php">create</a>
  <a href="crud/read.php">read</a>
